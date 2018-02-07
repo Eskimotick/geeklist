31941b25cbf85cf202ad3d41cb9fda26569f2be0
Matheus Fernandes <matheusfernandes@ejcm.com.br>
2018-02-07 15:34:08 -0200

    listando as listas

--- src/pages/jogo/jogo.ts
+++ src/pages/jogo/jogo.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Item } from '../item';
+import { List } from '../list';
 
 @Component({
   selector: 'page-jogo',
   templateUrl: 'jogo.html'
 })
 export class JogoPage {
+
+  itens: Item[] = [
+   { id: 1, name: 'The Legend of Zelda: Breath of the Wild'},
+   { id: 1, name: 'Super Mario Odissey'},
+   { id: 2, name: 'FIFA 18'},
+   { id: 2, name: 'Need for Speed Payback'}
+ ];
+
+  lists: List[] = [
+    { id: 1, name: 'Quero comprar esse ano'},
+    { id: 2, name: 'Emprestados do Bruno'}
+];
 
   constructor(public navCtrl: NavController) {
 
